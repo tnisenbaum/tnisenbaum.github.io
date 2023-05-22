0f10525e640fa0160a3cb7507f82c80703fe4642
tnisenbaum <tgallon1@jhu.edu>
2023-05-21 23:13:43 -0700

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -1,13 +1,31 @@
 jQuery(document).ready(function () {
-    jQuery(".main_tabs .tabs a").on("click", function (e) {
-      e.preventDefault();
-      switch (this.parentNode.id) {
-        case "python_examples":
-          window.location.href = "python_examples.html";
-          break;
-        case "SQL_examples":
-          window.location.href = "sql_examples.html";
-          break;
-      }
-    });
+  jQuery(".main_tabs .tabs a").on("click", function (e) {
+    e.preventDefault();
+    switch (this.parentNode.id) {
+      case "python_examples":
+        window.location.href = "python_examples.html";
+        break;
+      case "sql_examples":
+        window.location.href = "sql_examples.html";
+        break;
+      case "r_examples":
+        window.location.href = "r_examples.html";
+        break;
+      case "cgi_examples":
+        window.location.href = "cgi_examples.html";
+        break;
+      case "genome_examples":
+        window.location.href = "genome_examples.html";
+        break;
+      case "files_examples":
+        window.location.href = "files_examples.html";
+        break;
+      case "ngs_examples":
+        window.location.href = "ngs_examples.html";
+        break;
+      case "manuscripts_examples":
+        window.location.href = "manuscripts_examples.html";
+        break;
+    }
   });
+});
